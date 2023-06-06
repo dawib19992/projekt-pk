01037c0e0e0dde3a99f87d2e53f619ec3a55f08f
Dawid <115374337+dawib19992@users.noreply.github.com>
2023-06-06 21:17:46 +0200

    Update woltomierz.ino

--- woltomierz.ino
+++ woltomierz.ino
@@ -1,24 +1,27 @@
-#define inu A0 //wejscie analogowe 0 to wczytywane napiecie
+#define inu A0      //wejście analogowe 0 to wczytywane napięcie
+
 int uint;
 float ureal;
 
-void setup()
+void setup() 
 {
+  // put your setup code here, to run once:
   Serial.begin(9600);
   analogReference(DEFAULT);
   pinMode(inu, INPUT);
 }
 
-void loop()
+void loop() 
 {
+  // put your main code here, to run repeatedly:
+  
   uint = analogRead(inu);
-  ureal = uint * (5.0 / 1023.0); // Konwersja wartości analogowej na napięcie (zakładając maksymalne napięcie 5V)
-
-  Serial.print("Wartość wczytana w int: ");
+  Serial.print("Wartość wczytana jako int: ");
   Serial.println(uint);
   
-  Serial.print("Napięcie: ");
-  Serial.print(ureal);
+  ureal = uint * (5.0 / 1023.0);  // Przeliczenie wartości odczytanej na napięcie
+  Serial.print("Wartość wczytana jako napięcie: ");
+  Serial.print(ureal, 2);  // Wyświetlenie wartości z dwoma miejscami po przecinku
   Serial.println(" V");
   
   delay(500);
